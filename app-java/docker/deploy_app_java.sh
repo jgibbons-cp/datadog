5a37c5801ff23d4a8fb95300156a7e1a4aa72f33
Jenks Gibbons <jenksgibbons@gmail.com>
2021-04-28 13:51:38 -0700

    init push

--- app-java/docker/deploy_app_java.sh
+++ app-java/docker/deploy_app_java.sh
@@ -15,4 +15,4 @@ source ./create_network.sh
 # DD_TRACE_AGENT_PORT - Port number the Agent is listening on for configured host.
 
 #deploy app-java
-docker run --network $lab_network -p 8080:8080 --name TomcatContainer -e JAVA_OPTS='-javaagent:/usr/local/tomcat/lib/dd-java-agent.jar -Ddd.service=java-app -Ddd.version=.01 -Ddd.env=lab -Ddd.logs.injection=true -Ddd.profiling.enabled=true -XX:FlightRecorderOptions=stackdepth=256' -e DD_AGENT_HOST=dd-agent -e DD_TRACE_AGENT_PORT=8126 jgibbons-cp/app-java
+docker run -d --network $lab_network -p 8080:8080 --name TomcatContainer -e JAVA_OPTS='-javaagent:/usr/local/tomcat/lib/dd-java-agent.jar -Ddd.service=java-app -Ddd.version=.01 -Ddd.env=lab -Ddd.logs.injection=true -Ddd.profiling.enabled=true -XX:FlightRecorderOptions=stackdepth=256' -e DD_AGENT_HOST=dd-agent -e DD_TRACE_AGENT_PORT=8126 jgibbons-cp/app-java
