1fa6998fab6cc8379843eb7cf58d8358b1c9a068
Jenks Gibbons <jenksgibbons@gmail.com>
2021-10-31 15:03:08 -0700

    remove

--- docker/aspnet48_mvc_apm/aspnet48_mvc_win_server_core_ltsc_2019_app/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Datadog_APM_AspNet48_MVC_WinServerCoreLTSC2019.MvcApplication" Language="C#" %>
--- docker/aspnet48_mvc_apm/aspnet48_mvc_win_server_core_ltsc_2019_app/bin/Startup.ps1
+++
@@ -1,6 +0,0 @@
-﻿Set-Content -Path 'C:\\inetpub\\wwwroot\\datadog.json' -Value "{ `"DD_AGENT_HOST`": `"$env:DD_AGENT_HOST`" } "
-
-net stop /y was
-net start w3svc
-
-C:\\ServiceMonitor.exe w3svc
