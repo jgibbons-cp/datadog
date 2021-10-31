49ee57a73e15d98154f6f73d22652c816b0fc264
Jenks Gibbons <jenksgibbons@gmail.com>
2021-10-31 15:09:28 -0700

    init push

---
+++ docker/aspnet48_mvc_app/aspnet48_mvc_win_server_core_ltsc_2019_app/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Datadog_APM_AspNet48_MVC_WinServerCoreLTSC2019
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ docker/aspnet48_mvc_app/aspnet48_mvc_win_server_core_ltsc_2019_app/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Datadog_APM_AspNet48_MVC_WinServerCoreLTSC2019.MvcApplication" Language="C#" %>
