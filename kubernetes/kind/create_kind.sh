d2dca20e6833d500df2aa03d6a8e4a3f1f2afb2e
Jenks Gibbons <jenksgibbons@gmail.com>
2023-02-25 17:02:58 -0700

    dev push

--- kubernetes/kind/create_kind.sh
+++ kubernetes/kind/create_kind.sh
@@ -1,20 +1,28 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-#use cmd to see if docker is running
-docker ps > /dev/null
-
-if [ "$?" -eq "1" ];
+#use cmd to see if docker is running and get result string for later
+if ! rv=$(docker ps 2> /dev/null);
 then
-  echo "docker is not running..."
-  exit -1
+  echo "docker is not running... exiting..."
+  exit 1
 fi
 
 CLUSTER_NAME="kind-k8"
 
-#does not support merge of configs
-if test -f "config"; then
-  rm config
-fi
+#don't create if running
+if ! echo $rv | grep -E 'kind-k8-worker.*kind-k8-control-plane|kind-k8-control-plane.*kind-k8-worker' > /dev/null;
+then
+  #does not support merge of configs
+  if test -f "config"; then
+    rm config
+  fi
 
-#create
-kind create cluster --name "$CLUSTER_NAME" --kubeconfig config --config config.yaml
+  #create
+  if ! kind create cluster --name "$CLUSTER_NAME" --kubeconfig config --config config.yaml;
+  then
+    echo "Cluster failed to start... cleaning up and exiting..."
+    #seems to fail when start again even though no containers created
+    sh delete_kind.sh 2> /dev/null
+    exit 1
+  fi
+fi
