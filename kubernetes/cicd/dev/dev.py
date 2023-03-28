6be11a4355c49297470e3b8c3e7993081eef06fe
Jenks Gibbons <jenksgibbons@gmail.com>
2023-03-27 18:51:37 -0700

    merge response

--- kubernetes/cicd/dev/dev.py
+++ kubernetes/cicd/dev/dev.py
@@ -199,11 +199,21 @@ of {result["results"][0]["result"]["step_count_total"]} steps. Exiting..."""
         send_log("Success", service)
     delete_kind_cluster(kind_dir)
 
+    return_string = ""
     os.system('git config --global user.email "jenks.gibbons@datadoghq.com"')
     os.system('git config --global user.name "Jenks"')
     os.system("git fetch --unshallow")
-    msg = subprocess.check_output("git checkout main && git merge --no-ff --allow-unrelated-histories origin/dev-app-java && git push", shell=True, text=True)
-    send_log(msg, "app-java")
+    try:
+        return_string = subprocess.check_output("git checkout main && git merge --no-ff \
+                                    --allow-unrelated-histories \
+                                    origin/dev-app-java && git push", \
+                                    shell=True, text=True)
+        return_string = f"INFO {return_string}"
+    except CalledProcessError as error:
+        return_string = str(error)
+        return_string = f"ERROR {return_string}"
+        
+    send_log(msg, service)
     
     local_module = config_data["modules"]["cicd"]
     os.remove(f"{script_directory}/{local_module}")
