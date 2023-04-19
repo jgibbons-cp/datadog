418bf875e30d007c30c76b359f45bfb8efea4eb9
Jenks Gibbons <jenksgibbons@gmail.com>
2023-04-19 16:49:53 -0700

    ecs support

---
+++ docker/aspnet48_mvc_app/aspnet48_mvc_win_server_core_ltsc_2019_app/Startup_ecs.ps1
@@ -0,0 +1,8 @@
+﻿$private_ip = $(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+$private_ip = $private_ip.Content
+Set-Content -Path 'C:\\inetpub\\wwwroot\\datadog.json' -Value "{ `"DD_AGENT_HOST`": `"$private_ip`" } "
+
+net stop /y was
+net start w3svc
+
+C:\\ServiceMonitor.exe w3svc
