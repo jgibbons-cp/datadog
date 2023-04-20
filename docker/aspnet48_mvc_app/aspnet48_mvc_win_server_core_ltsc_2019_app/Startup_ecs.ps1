4e0a8e505b927fee23687582b51bf16ed465d76e
Jenks Gibbons <jenksgibbons@gmail.com>
2023-04-20 13:00:37 -0700

    ecs support

--- docker/aspnet48_mvc_app/aspnet48_mvc_win_server_core_ltsc_2019_app/Startup_ecs.ps1
+++ docker/aspnet48_mvc_app/aspnet48_mvc_win_server_core_ltsc_2019_app/Startup_ecs.ps1
@@ -1,4 +1,10 @@
-﻿$private_ip = $(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+﻿# from IAM roles for task container bootstrap script at https://docs.aws.amazon.com/AmazonECS/latest/developerguide/windows_task_IAM_roles.html
+$gateway = (Get-NetRoute | Where { $_.DestinationPrefix -eq '0.0.0.0/0' } | Sort-Object RouteMetric | Select NextHop).NextHop
+$ifIndex = (Get-NetAdapter -InterfaceDescription "Hyper-V Virtual Ethernet*" | Sort-Object | Select ifIndex).ifIndex
+New-NetRoute -DestinationPrefix 169.254.170.2/32 -InterfaceIndex $ifIndex -NextHop $gateway -PolicyStore ActiveStore # credentials API
+New-NetRoute -DestinationPrefix 169.254.169.254/32 -InterfaceIndex $ifIndex -NextHop $gateway -PolicyStore ActiveStore # metadata API
+
+$private_ip = $(curl -UseBasicParsing http://169.254.169.254/latest/meta-data/local-ipv4)
 $private_ip = $private_ip.Content
 Set-Content -Path 'C:\\inetpub\\wwwroot\\datadog.json' -Value "{ `"DD_AGENT_HOST`": `"$private_ip`" } "
 
