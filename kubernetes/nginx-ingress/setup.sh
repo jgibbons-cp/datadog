ef2ddc2fd61fba9d70c51c6bf0b59c35adac820b
Jenks Gibbons <jenksgibbons@gmail.com>
2023-05-09 16:12:25 -0700

    init

--- kubernetes/nginx-ingress/setup.sh
+++ kubernetes/nginx-ingress/setup.sh
@@ -3,17 +3,22 @@
 SUCCESS=0
 MAX_TRIES=5
 MAX=4
-counter=0
 INCREMENT=1
 
 # create the ingress
-cmd() {
+create_ingress() {
     kubectl create -f application_ingress.yaml &>/dev/null
 }
 
 # make sure you can reach cluster
 if ! kubectl get pods &> /dev/null; then
-    echo "No cluster configured... exiting...\n"
+    echo "\nNo cluster configured... exiting...\n"
+    exit 1
+fi
+
+# need cli and to be logged in
+if ! az account show &> /dev/null; then
+    echo "\nDepending on the error either install the Azure cli or login with 'az login'... exiting...\n"
     exit 1
 fi
 
@@ -40,6 +45,26 @@ helm install ingress-nginx ingress-nginx/ingress-nginx --create-namespace --name
 --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-health-probe-request-path"=/healthz \
 --set controller.service.loadBalancerSourceRanges="{$public_ip/32}"
 
+# can't do anything without a public ip
+ip=""
+counter=0
+SLEEP_TIME=30
+while [ "$ip" == "" ] && [ "$counter" -lt $MAX_TRIES ]; do
+  echo "\n\nNeed a public IP, waiting $SLEEP_TIME seconds."
+  sleep $SLEEP_TIME
+  counter=$((counter + $INCREMENT))
+  ip=$(kubectl get svc ingress-nginx-controller -n ingress-nginx  -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+
+  # no public ip after 2.5 minutes then let's bail
+  if [ "$ip" = "" ] && [ "$counter" == $MAX ]; then
+    echo "\n\nFailed to get a public IP... rolling back and exiting...\n"
+    sh teardown.sh
+    exit 1
+  fi
+done
+
+echo "\n\nGot public IP $ip.\n\n"
+
 # get az username for public dns for lb
 principal_username=$(az ad signed-in-user show --query "userPrincipalName" | tr -d '"' | tr -d "." | sed 's/@.*//')
 kubectl annotate svc -n ingress-nginx ingress-nginx-controller service.beta.kubernetes.io/azure-dns-label-name=$principal_username
@@ -53,12 +78,16 @@ fqdn=$principal_username.$azure_region$rest_fqdn
 sed -i .bak "s/<FQDN>/$fqdn/" application_ingress.yaml
 
 # install ingress - 4 retries
-cmd
-while [ "$?" != $SUCCESS ] && [ "$counter" -lt $MAX_TRIES ]; do
+ret_val=0
+create_ingress
+ret_val=$?
+counter=0
+while [ "$ret_val" != $SUCCESS ] && [ "$counter" -lt $MAX_TRIES ]; do
   counter=$((counter + $INCREMENT))
-  cmd
-
-  if [ "$?" != $SUCCESS ] && [ "$counter" == $MAX ]; then
+  create_ingress
+  ret_val=$?
+  
+  if [ "$ret_val" != $SUCCESS ] && [ "$counter" == $MAX ]; then
     echo "Ingress failed to create... run 'kubectl create -f application_ingress.yaml' again or teardown.sh.\n"
   fi
 done
