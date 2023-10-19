8628d818cb8ff6012bc61a8641a790e76df69220
Jenks Gibbons <jenksgibbons@gmail.com>
2023-10-19 14:59:28 -0700

    multiplatform build support

--- app-java/docker/build.sh
+++ app-java/docker/build.sh
@@ -81,16 +81,16 @@ mvn clean && mvn compile && mvn package
 
 #if want otel tracing
 OTEL=0
-DD_TRACER=1
+DD_TRACER=0
 
 if [ "$OTEL" = 1 ]; then
   wget -O otel-java-tracer.jar https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/latest/download/opentelemetry-javaagent.jar
-  docker buildx build --platform linux/amd64,linux/arm64 --push -t jenksgibbons/app-java -f docker/Dockerfile.tomcat --build-arg TRACER=otel-java-tracer.jar .
+  docker buildx build --platform linux/amd64,linux/arm64 --push -t jenksgibbons/app-java:otel_tracer -f docker/Dockerfile.tomcat --build-arg TRACER=otel-java-tracer.jar .
 elif [ "$DD_TRACER" = 1 ]; then
   wget -O dd-java-tracer.jar https://dtdg.co/latest-java-tracer
-  docker buildx build --platform linux/amd64,linux/arm64 --push -t jenksgibbons/app-java -f docker/Dockerfile.tomcat --build-arg TRACER=dd-java-tracer.jar .
+  docker buildx build --platform linux/amd64,linux/arm64 --push -t jenksgibbons/app-java:dd_tracer -f docker/Dockerfile.tomcat --build-arg TRACER=dd-java-tracer.jar .
 else
-  docker buildx build --platform linux/amd64,linux/arm64 --push -t jenksgibbons/app-java -f docker/Dockerfile.tomcat --build-arg TRACER= .
+  docker buildx build --platform linux/amd64,linux/arm64 --push -t jenksgibbons/app-java:no_tracer -f docker/Dockerfile.tomcat --build-arg TRACER= .
 fi
 
 if [ ! -d "initdb" ]
