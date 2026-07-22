ea0e5a246af431caba692e97b431bfdd71d24785
Jenks Gibbons <jenksgibbons@gmail.com>
2026-07-22 16:36:02 -0700

    get storage account to delete/remove file, fix app delettion

--- logs/byoc/aks/cleanup_lab.sh
+++ logs/byoc/aks/cleanup_lab.sh
@@ -2,12 +2,20 @@
 
 source ./.env
 
+# can't reuse storage name for 14 days without UI purge so need to grab it
+byoc_logs_storage_account=".byoc_logs_storage_account"
+BYOC_LOGS_STORAGE_ACCOUNT=$(cat $(pwd)/"$byoc_logs_storage_account")
+rm $byoc_logs_storage_account
+
 az storage account delete -n "$BYOC_LOGS_STORAGE_ACCOUNT" -g "$BYOC_LOGS_RESOURCE_GROUP" --yes
 
-service_principal=$(az ad sp list --filter "displayName eq 'byoclogs'" --query "[].appId" -o tsv)
-app_id=$(az ad app list --display-name byoclogs --query "[].appId" -o tsv)
+app_id=$(az ad app list --filter "displayName eq '$BYOC_LOGS_STORAGE_ACCOUNT'" --query "[].appId" -o tsv)
 if [[ -n "$app_id" ]]; then
-  az ad sp delete --id "$service_principal"
+  az ad app delete --id "$app_id"
+fi
+
+app_id=$(az ad app list --filter "displayName eq 'byoclogs'" --query "[].appId" -o tsv)
+if [[ -n "$app_id" ]]; then
   az ad app delete --id "$app_id"
 fi
 
