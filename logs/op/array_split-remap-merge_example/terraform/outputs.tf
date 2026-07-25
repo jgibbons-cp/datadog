6e9bde506d922fbbebb6de39b46563cb789b7db8
Jenks Gibbons <jenksgibbons@gmail.com>
2026-07-24 17:14:12 -0700

    change name

--- logs/op/array_split-remap-merge_example/terraform/outputs.tf
+++ logs/op/array_split-remap-merge_example/terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "pipeline_id" {
-  value       = datadog_observability_pipeline.api_demo.id
+  value       = datadog_observability_pipeline.opw_demo.id
   description = "Pass to OPW as DD_OP_PIPELINE_ID"
 }
